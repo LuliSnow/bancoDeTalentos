891740b595d626cce606e85338ba0eb73400f839
LuliSnow <luisa.santana.neves@gmail.com>
2022-05-10 10:47:25 -0300

    Conexão do frontend com DB heroku

--- app-bancodetalentos/src/app/cadastro.service.ts
+++ app-bancodetalentos/src/app/cadastro.service.ts
@@ -9,25 +9,25 @@ export class CadastroService {
   constructor(private http: HttpClient) { }
 
   create(data: any) {
-    let url = 'http://localhost:8080/talentos';
+    let url = 'https://app-banco-talentos.herokuapp.com/talentos';
     return this.http.post(url, data);
   }
 
   getAll() {
-    let url = 'http://localhost:8080/talentos';
+    let url = 'https://app-banco-talentos.herokuapp.com/talentos';
     return this.http.get(url);
   }
 
   getOne(idtalento: number) {
-    return this.http.get(`http://localhost:8080/talentos/${idtalento}`)
+    return this.http.get(`https://app-banco-talentos.herokuapp.com/talentos/${idtalento}`)
   }
 
   alterar(dados: any) {
-    let url = `http://localhost:8080/talentos/${dados.id}`
+    let url = `https://app-banco-talentos.herokuapp.com/talentos/${dados.id}`
     return this.http.put(url, dados)
   }
 
   apagar(idtalento: number) {
-    return this.http.delete(`http://localhost:8080/talentos/${idtalento}`)
+    return this.http.delete(`https://app-banco-talentos.herokuapp.com/talentos/${idtalento}`)
   }
 }
